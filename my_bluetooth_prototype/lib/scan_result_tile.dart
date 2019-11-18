8d768c0b70df957f4021b5d321307df669b34439
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2019-11-18 12:17:21 +0100

    Update ScanResultTile

--- my_bluetooth_prototype/lib/scan_result_tile.dart
+++ my_bluetooth_prototype/lib/scan_result_tile.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 class ScanResultTile extends StatelessWidget {
-  const ScanResultTile({Key key, this.result, this.onTap}) : super(key: key);
+  const ScanResultTile({
+    Key key,
+    this.result,
+    this.onTap,
+  }) : super(key: key);
 
   final ScanResult result;
   final VoidCallback onTap;
@@ -30,21 +34,16 @@ class ScanResultTile extends StatelessWidget {
 
   Widget _buildAdvRow(BuildContext context, String title, String value) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(title, style: Theme.of(context).textTheme.caption),
-          SizedBox(
-            width: 12.0,
-          ),
+          const SizedBox(width: 12.0),
           Expanded(
             child: Text(
               value,
-              style: Theme.of(context)
-                  .textTheme
-                  .caption
-                  .apply(color: Colors.black),
+              style: _buildTextStyle(context),
               softWrap: true,
             ),
           ),
@@ -53,8 +52,12 @@ class ScanResultTile extends StatelessWidget {
     );
   }
 
+  TextStyle _buildTextStyle(BuildContext context) {
+    return Theme.of(context).textTheme.caption.apply(color: Colors.black);
+  }
+
   String getNiceHexArray(List<int> bytes) {
-    return '[${bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]'
+    return '[${bytes.map((int i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]'
         .toUpperCase();
   }
 
@@ -62,11 +65,14 @@ class ScanResultTile extends StatelessWidget {
     if (data.isEmpty) {
       return null;
     }
-    List<String> res = [];
-    data.forEach((id, bytes) {
-      res.add(
-          '${id.toRadixString(16).toUpperCase()}: ${getNiceHexArray(bytes)}');
-    });
+    List<String> res = <String>[];
+    data.forEach(
+      (int id, List<int> bytes) {
+        res.add(
+          '${id.toRadixString(16).toUpperCase()}: ${getNiceHexArray(bytes)}',
+        );
+      },
+    );
     return res.join(', ');
   }
 
@@ -74,10 +80,12 @@ class ScanResultTile extends StatelessWidget {
     if (data.isEmpty) {
       return null;
     }
-    List<String> res = [];
-    data.forEach((id, bytes) {
-      res.add('${id.toUpperCase()}: ${getNiceHexArray(bytes)}');
-    });
+    List<String> res = <String>[];
+    data.forEach(
+      (String id, List<int> bytes) {
+        res.add('${id.toUpperCase()}: ${getNiceHexArray(bytes)}');
+      },
+    );
     return res.join(', ');
   }
 
@@ -87,30 +95,40 @@ class ScanResultTile extends StatelessWidget {
       title: _buildTitle(context),
       leading: Text(result.rssi.toString()),
       trailing: RaisedButton(
-        child: Text('CONNECT'),
+        child: const Text('CONNECT'),
         color: Colors.black,
         textColor: Colors.white,
         onPressed: (result.advertisementData.connectable) ? onTap : null,
       ),
       children: <Widget>[
         _buildAdvRow(
-            context, 'Complete Local Name', result.advertisementData.localName),
-        _buildAdvRow(context, 'Tx Power Level',
-            '${result.advertisementData.txPowerLevel ?? 'N/A'}'),
+          context,
+          'Complete Local Name',
+          result.advertisementData.localName,
+        ),
         _buildAdvRow(
-            context,
-            'Manufacturer Data',
-            getNiceManufacturerData(
-                    result.advertisementData.manufacturerData) ??
-                'N/A'),
+          context,
+          'Tx Power Level',
+          '${result.advertisementData.txPowerLevel ?? 'N/A'}',
+        ),
         _buildAdvRow(
-            context,
-            'Service UUIDs',
-            (result.advertisementData.serviceUuids.isNotEmpty)
-                ? result.advertisementData.serviceUuids.join(', ').toUpperCase()
-                : 'N/A'),
-        _buildAdvRow(context, 'Service Data',
-            getNiceServiceData(result.advertisementData.serviceData) ?? 'N/A'),
+          context,
+          'Manufacturer Data',
+          getNiceManufacturerData(result.advertisementData.manufacturerData) ??
+              'N/A',
+        ),
+        _buildAdvRow(
+          context,
+          'Service UUIDs',
+          (result.advertisementData.serviceUuids.isNotEmpty)
+              ? result.advertisementData.serviceUuids.join(', ').toUpperCase()
+              : 'N/A',
+        ),
+        _buildAdvRow(
+          context,
+          'Service Data',
+          getNiceServiceData(result.advertisementData.serviceData) ?? 'N/A',
+        ),
       ],
     );
   }
