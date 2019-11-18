5d04ed4f39e2ec907805f7fdde53cf03aa18b5c6
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2019-11-18 11:48:47 +0100

    Create new class DeviceScreen, bluetoothOffScreen, FindDevicesScreen

---
+++ my_bluetooth_prototype/lib/bluetooth_off_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_blue/flutter_blue.dart';
+
+/// A screen that displays that the bluetooth-connectivity is off
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({Key key, this.state}) : super(key: key);
+
+  final BluetoothState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'Bluetooth Adapter is ${state.toString().substring(15)}.',
+              style: _buildStyle(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextStyle _buildStyle(BuildContext context) {
+    return Theme.of(context)
+        .primaryTextTheme
+        .subhead
+        .copyWith(color: Colors.white);
+  }
+}
