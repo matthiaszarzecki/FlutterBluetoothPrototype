a39ebaf9c03f79ac83c36125af1907c8b60efc0d
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2019-11-18 12:18:49 +0100

    Update ServiceTile

--- my_bluetooth_prototype/lib/service_tile.dart
+++ my_bluetooth_prototype/lib/service_tile.dart
@@ -4,11 +4,14 @@ import 'package:flutter_blue/flutter_blue.dart';
 import 'package:my_bluetooth_prototype/characteristic_tile.dart';
 
 class ServiceTile extends StatelessWidget {
+  const ServiceTile({
+    Key key,
+    this.service,
+    this.characteristicTiles,
+  }) : super(key: key);
+
   final BluetoothService service;
   final List<CharacteristicTile> characteristicTiles;
-
-  const ServiceTile({Key key, this.service, this.characteristicTiles})
-      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +21,28 @@ class ServiceTile extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Service'),
-            Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}',
-                style: Theme.of(context)
-                    .textTheme
-                    .body1
-                    .copyWith(color: Theme.of(context).textTheme.caption.color))
+            const Text('Service'),
+            Text(
+              '0x${service.uuid.toString().toUpperCase().substring(4, 8)}',
+              style: _buildTextStyle(context),
+            )
           ],
         ),
         children: characteristicTiles,
       );
     } else {
       return ListTile(
-        title: Text('Service'),
-        subtitle:
-            Text('0x${service.uuid.toString().toUpperCase().substring(4, 8)}'),
+        title: const Text('Service'),
+        subtitle: Text(
+          '0x${service.uuid.toString().toUpperCase().substring(4, 8)}',
+        ),
       );
     }
+  }
+
+  TextStyle _buildTextStyle(BuildContext context) {
+    return Theme.of(context).textTheme.body1.copyWith(
+          color: Theme.of(context).textTheme.caption.color,
+        );
   }
 }
